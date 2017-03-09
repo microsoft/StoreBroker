2a86150d134d3f0a1552b00b199dd252a6092a04
Howard Wolosky <HowardWolosky@users.noreply.github.com>
2017-03-08 17:34:12 -0800

    Enable relative package paths for uploading/downloading
    PowerShell-native commands can work just fine with relative file
    paths, however when interacting with other code, it's necessary
    to use `Convert-Path` to first resolve the path to its full path.
    
    `Set-SubmissionPackage` / `Get-SubmissionPackage` use the Azure
    assemblies for uploading/downloading packages, and thus need to
    have `$PackagePath` resolved to its full path in the even that
    users are providing relative paths to the command.
    
    `Resolve-Path` only works to resolve relative paths if they actually
    exist.  So, I'm introducing a new helper, `Resolve-UnverifiedPath`
    that will resolve a relative path, regardless of whether or not
    it exists.
    
    Additionally, fixing the telemetry exception bucket name used
    for `Get-SubmissionPackage`.

--- StoreBroker/Helpers.ps1
+++ StoreBroker/Helpers.ps1
@@ -756,3 +756,60 @@ function Write-InteractiveHost
         Write-Host @PSBoundParameters
     }
 }
+
+function Resolve-UnverifiedPath
+{
+<#
+    .SYNOPSIS
+        A wrapper around Resolve-Path that works for paths that exist as well
+        as for paths that don't (Resolve-Path normally throws an exception if
+        the path doesn't exist.)
+
+    .DESCRIPTION
+        A wrapper around Resolve-Path that works for paths that exist as well
+        as for paths that don't (Resolve-Path normally throws an exception if
+        the path doesn't exist.)
+
+        The Git repo for this module can be found here: https://aka.ms/StoreBroker
+
+    .EXAMPLE
+        Resolve-UnverifiedPath -Path 'c:\windows\notepad.exe'
+
+        Returns the string 'c:\windows\notepad.exe'.
+
+    .EXAMPLE
+        Resolve-UnverifiedPath -Path '..\notepad.exe'
+
+        Returns the string 'c:\windows\notepad.exe', assuming that it's executed from
+        within 'c:\windows\system32' or some other sub-directory.
+
+    .EXAMPLE
+        Resolve-UnverifiedPath -Path '..\foo.exe'
+
+        Returns the string 'c:\windows\foo.exe', assuming that it's executed from
+        within 'c:\windows\system32' or some other sub-directory, even though this
+        file doesn't exist.
+
+    .OUTPUTS
+        [string] - The fully resolved path 
+
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Position=0,
+            ValueFromPipeline)]
+        [string] $Path
+    )
+
+    $resolvedPath = Resolve-Path -Path $Path -ErrorVariable resolvePathError -ErrorAction SilentlyContinue
+
+    if ($null -eq $resolvedPath)
+    {
+        return $resolvePathError[0].TargetObject
+    }
+    else
+    {
+        return $resolvedPath.Path
+    }
+}

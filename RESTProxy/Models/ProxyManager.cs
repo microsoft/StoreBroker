3a0aa5d36b0e7e35acc899988ed6ff17185b3f61
Pepe Rivera <joriv@microsoft.com>
2022-01-19 14:35:07 -0800

    Fix more issues

--- RESTProxy/Models/ProxyManager.cs
+++ RESTProxy/Models/ProxyManager.cs
@@ -117,8 +117,6 @@ namespace Microsoft.Windows.Source.StoreBroker.RestProxy.Models
             {
                 ProxyManager.defaultTenantId = defaultTenantId.ToLowerInvariant();
             }
-
-            ServicePointManager.CheckCertificateRevocationList = true;
         }
 
         /// <summary>
